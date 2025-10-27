0f35be9ebf2d12524f9e0e6fd505f16a0887796c
jheintre-sketch <jheintre@gmail.com>
2025-10-27 02:56:09 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+// Service Worker pour PWA
+const CACHE_NAME = 'muscu-planner-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Installation
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
